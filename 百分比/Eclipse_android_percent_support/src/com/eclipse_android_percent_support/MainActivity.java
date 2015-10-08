e71a20ac3ea6e553e6d407775fbfd7c72dabbaca
tinghaoMa <mth_itck@163.com>
2015-10-08 15:09:01 +0800

    百分比布局适配

---
+++ 百分比/Eclipse_android_percent_support/src/com/eclipse_android_percent_support/MainActivity.java
@@ -0,0 +1,18 @@
+package com.eclipse_android_percent_support;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	
+
+}
