80e8e051d590c83e34576fcc314fe4458529e4f5
马廷皓 <mth_itck@163.com>
2016-05-14 13:41:08 +0800

    test ssh key  second

--- Messenger_Handler_DEMO/app/src/main/java/mth/constants/Constants.java
+++ Messenger_Handler_DEMO/app/src/main/java/mth/constants/Constants.java
@@ -2,6 +2,7 @@ package mth.constants;
 
 
 public class Constants {
+	
     public static final int SERVICE_CONNECT_SUCESS = 0x001;
     public static final int MSG_CLIENT = 0x002;
     public static final int MSG_DEFAULT = 0x003;
